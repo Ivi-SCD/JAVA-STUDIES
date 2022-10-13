c46077d6ba93344977d8ba015bb94edae79dcc86
Ivi-SCD <ivipnascimento@hotmail.com>
2022-10-13 18:12:45 -0300

    Uploaded Interfaces Exercise One

---
+++ JAVA-POO/INTERFACES1/src/model/services/BrazilTaxService.java
@@ -0,0 +1,9 @@
+package model.services;
+
+public class BrazilTaxService implements TaxService{
+	
+	public double tax(double amount) {
+		double tax  = (amount <= 100) ? amount *0.2 : amount * 0.15;
+		return tax;
+	}
+}
