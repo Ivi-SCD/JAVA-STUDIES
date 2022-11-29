5cf921122d44cebf8477bb839079939e97df99aa
Ivi <ivipnascimento@hotmail.com>
2022-11-29 11:32:43 -0300

    BeforeEach, AfterEach, BeforeAll, AfterAll anotations JUnit

--- JAVA-ALURA/01 - ARQUITETURA&DESIGN/Testes/JUnitTDD1/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ JAVA-ALURA/01 - ARQUITETURA&DESIGN/Testes/JUnitTDD1/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
@@ -13,9 +17,32 @@ import br.com.alura.tdd.service.bonus.RegraBonusPorTrabalho;
 
 class BonusServiceTest {
 
+	private BonusService service;
+	
+	@BeforeEach
+	private void inicializar() {
+		service = new BonusService();
+		System.out.println("Iniciando thread.");
+	}
+	
+	@BeforeAll
+	private static void beforeAllMethod() {
+		System.out.println("Iniciando...");
+	}
+	
+	@AfterEach
+	private void finalizar() {
+		System.out.println("Finalizando thread.");
+	}
+	
+	@AfterAll
+	private static void afterAllMethod() {
+		System.out.println("Encerrando...");
+	}
+	
 	@Test
 	void bonusZeradoParaSalariosAcimaDeDezMil() {
-		BonusService service = new BonusService();
+		
 		assertThrows(IllegalArgumentException.class, () -> service.calcularBonus
 				(new Funcionario("Ivisson", LocalDate.now(), new BigDecimal("25000")),
 						new RegraBonusPorTrabalho()));
@@ -32,7 +59,7 @@ class BonusServiceTest {
 	
 	@Test
 	void bonusDeveriaSerDezPorCentoDoSalario() {
-		BonusService service = new BonusService();
+		
 		BigDecimal bonus = service.calcularBonus
 		(new Funcionario("Ivisson", LocalDate.now(), new BigDecimal("2500")),
 				new RegraBonusPorTrabalho());
@@ -42,7 +69,7 @@ class BonusServiceTest {
 	
 	@Test
 	void bonusDeveriaSerDezPorCentoParaSalarioMil() {
-		BonusService service = new BonusService();
+		
 		BigDecimal bonus = service.calcularBonus
 		(new Funcionario("Ivisson", LocalDate.now(), new BigDecimal("10000")),
 				new RegraBonusPorTrabalho());
